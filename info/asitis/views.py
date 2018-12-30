bfc488ad51715cf21e4e02fbe4765263f013a6aa
HSH4U2U <hsh700788@naver.com>
2018-12-30 14:38:35 +0900

    search update

--- info/asitis/views.py
+++ info/asitis/views.py
@@ -8,8 +8,8 @@ from django.db.models import Q
 # 이미 importing 작업 끝냄
 # importing_class_list()
 
-
 def base(request):
+    # 족보 search 기능 구현(등록과 검색 따로 받음)
     if "search" in request.GET:
         search_term = request.GET["search"]
         if search_term:
@@ -31,3 +31,28 @@ def base(request):
         return render(request, 'asitis/search.html', ctx)
     ctx = {}
     return render(request, 'asitis/base.html', ctx)
+
+
+def detail(request):
+    # 족보 search 기능 구현(등록과 검색 따로 받음)
+    if "search" in request.GET:
+        search_term = request.GET["search"]
+        if search_term:
+            subjects = Subject.objects.filter(
+                Q(name__icontains=search_term) |
+                Q(professor__icontains=search_term)
+            ).distinct().order_by('name')
+            for subject in subjects:
+                subject.name = list(subject.name.partition(search_term))
+                subject.professor = list(subject.professor.partition(search_term))
+            # select 무엇이 되었는지 구별(족보 등록인지 족보 검색인지)
+            if "select" in request.GET:
+                select = request.GET["select"]
+        ctx = {
+            'select': select,
+            'search_term': search_term,
+            'subjects': subjects
+        }
+        return render(request, 'asitis/search.html', ctx)
+    ctx = {}
+    return render(request, 'asitis/detail.html', ctx)
