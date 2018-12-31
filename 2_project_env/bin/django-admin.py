4c10807bda60e66f241fe999c191fda448208e33
상현 <hwangsanghyun@sanghyeon-ui-MacBookPro.local>
2018-12-31 14:59:06 +0900

    mac

---
+++ 2_project_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hwangsanghyun/dev/projects/2_project/2_project_env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
